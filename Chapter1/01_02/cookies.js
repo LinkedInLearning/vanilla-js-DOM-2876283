75e455dc900ece93aba77880f2620dcb3ffb4184
Scott Means <smeans@gmail.com>
2021-03-22 16:11:35 +0700

    completed cookie ingredient application for chapter 1 video 2

--- Chapter1/01_02/cookies.js
+++ Chapter1/01_02/cookies.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    alert('Hello, cookies!');
+    const ingredients = document.getElementById('ingredients');
+
+    let total = 0;
+
+    Array.prototype.forEach.call(ingredients.children, (tr) => {
+        const td = tr.children[1];
+
+        if (tr.id == 'totals') {
+            td.innerText = total;
+        } else {
+            const weight = parseFloat(td.innerText);
+
+            total += weight;
+        }
+    });
 });
