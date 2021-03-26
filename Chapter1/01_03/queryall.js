1c52b1f94f4f953842c746b7e0a5d69a568e6193
Scott Means <smeans@gmail.com>
2021-03-26 14:33:06 +0700

    completed queryall demo for chapter 1 video 3

--- Chapter1/01_03/queryall.js
+++ Chapter1/01_03/queryall.js
@@ -7,6 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
         refreshOutput(e.target.value);
     });
 
+    document.forms.queryall.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        refreshQuery(e.target.selector.value);
+
+        return false;
+    });
+
     refreshOutput(textarea.value);
 });
 
@@ -14,4 +22,18 @@ function refreshOutput(html) {
     const output = document.querySelector('#output');
 
     output.innerHTML = html;
+}
+
+function refreshQuery(selector) {
+    const output = document.querySelector('#output');
+    const matches = document.querySelector('#matches');
+
+    matches.innerHTML = '';
+
+    output.querySelectorAll(selector).forEach((el) => {
+        const li = document.createElement('li');
+        li.innerText = el.outerHTML;
+
+        matches.appendChild(li);
+    });
 }
