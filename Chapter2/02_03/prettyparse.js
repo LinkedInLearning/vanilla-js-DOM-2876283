c29f27f3e60da309fa5ee20a0c7f4f03521a452f
Scott Means <smeans@gmail.com>
2021-04-07 13:40:43 +0700

    ending state of chapter 2 video 3 code

--- Chapter2/02_03/prettyparse.js
+++ Chapter2/02_03/prettyparse.js
@@ -14,18 +14,93 @@ function refreshOutput(html) {
     const output = document.querySelector('pp-output');
 
     output.innerText = '';
-    output.appendChild(prettyParse(html));
+    testLexer(html);
 }
 
-function prettyParse(html) {
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
+class Lexer {
+    constructor(source) {
+        this.source = source;
+        this.file_pointer = 0;
+    }
 
-    div.innerHTML = html;
+    read() {
+        if (this.file_pointer < 0 || this.file_pointer >= this.source.length) {
+            return undefined;
+        }
 
-    div.childNodes.forEach((node) => {
-        fragment.appendChild(node);
-    });
+        return this.source[this.file_pointer++];
+    }
 
-    return fragment;
+    rewind() {
+        this.file_pointer = 0;
+    }
+
+    match(token) {
+        return this.remainder.search(token) === 0;
+    }
+
+    consumeMatch(token) {
+        const match = this.remainder.match(token);
+
+        if (match && match.length && match.index == 0) {
+            this.file_pointer += match[0].length;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    readUntil(condition) {
+        const start_pointer = this.file_pointer;
+
+        while (!this.eof && !condition(this)) {
+            this.file_pointer++;
+        }
+
+        return this.source.substring(start_pointer, this.file_pointer);
+    }
+
+    readIdentifier() {
+        return this.readUntil((lexer) => !lexer.match(/\w/));
+    }
+
+    skipWhitespace() {
+        return this.readUntil((lexer) => !lexer.match(/\s/));
+    }
+
+    get eof() {
+        return this.file_pointer >= this.source.length;
+    }
+
+    get remainder() {
+        return this.source.substring(this.file_pointer);
+    }
+}
+
+function testLexer(html) {
+    const lexer = new Lexer(html);
+
+    let output = '';
+
+    while (!lexer.eof) {
+        output += lexer.read();
+    }
+
+    console.assert(html == output);
+
+    lexer.rewind();
+
+    console.assert(lexer.match('<!--'));
+    console.assert(!lexer.match('random text'));
+
+    console.assert(lexer.consumeMatch('<!--'));
+    const comment = lexer.readUntil((lexer) => lexer.match('-->'));
+    lexer.consumeMatch('-->');
+    console.log('comment: ', comment);
+
+    lexer.skipWhitespace();
+    console.assert(lexer.consumeMatch('<'));
+    const tag = lexer.readIdentifier();
+    console.log(tag);
 }
